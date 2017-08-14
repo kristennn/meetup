eeec824e760e3319d5fca889b4ac3ff145cf5cd6
13949836783@163.com <13949836783@163.com>
2017-08-14 15:09:11 +0800

    add basic styling

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -1,7 +1,30 @@
 class MeetsController < ApplicationController
 
+  def index
+    @meets = Meet.all
+  end
+
   def new
     @meet = Meet.new
+  end
+
+  def show
+    @meet = Meet.find(params[:id])
+  end
+
+  def create
+    @meet = Meet.new(meet_params)
+    if @meet.save
+      redirect_to meets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def meet_params
+    params.require(:meet).permit(:title, :info)
   end
 
 end
